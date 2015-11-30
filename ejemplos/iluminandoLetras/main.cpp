91b1aca96dfb121176e1773895d99397ee41f116
juanAFernandez <juan.antonio.fernandez.sanchez.88@gmail.com>
2015-11-30 19:20:27 +0100

    Añadiendo mejoras

--- ejemplos/iluminandoLetras/main.cpp
+++ ejemplos/iluminandoLetras/main.cpp
@@ -20,53 +20,123 @@ void fill_pixel(neopixel::Pixel buffer[25], int x, int y, int red, int green, in
     buffer[posicion].blue=blue;
     
 }
+
+void void_matrix(neopixel::Pixel aux[25], int tam=25){
+    
+    for(int i=0;i<tam;i++){
+        aux[i].red=0;
+        aux[i].green=0;
+        aux[i].blue=0;
+    }
+}
+
 //r: intensidadRojo
 //g: intensidadVerde
 //b: intensidadAzul
-void generaLetra(neopixel::Pixel vector[], char letra, int r, int g, int b){
+void generaLetra(neopixel::Pixel vector[], char letra, int red, int green, int blue){
 
-    switch(letra){
+    /*
+    * Cada letra tiene 5 valores en hexadecimal que representan en binario cada fila de la matrix 5x5 (aunque sobren 3 bits)
+    */
+    unsigned char espacio[5]={0x0, 0x0, 0x0, 0x0, 0x0};
+    unsigned char a[5]={0x88, 0x88, 0xF8, 0x88, 0x70};
+    unsigned char b[5]={0xF0, 0x88, 0xF0, 0x88, 0xF0};      
+    unsigned char c[5]={0x78, 0x80, 0x80, 0x80, 0x78};   
+    unsigned char d[5]={0xF0, 0x88, 0x88, 0x88, 0xF0};
+    unsigned char e[5]={0xF8, 0x80, 0xF8, 0x80, 0xF8};
+    unsigned char f[5]={0x80, 0x80, 0xE0, 0x80, 0xF8};    
+    unsigned char g[5]={0x78, 0x88, 0xB8, 0x80, 0x78};
+    unsigned char h[5]={0x88, 0x88, 0xF8, 0x88, 0x88};
+    unsigned char i[5]={0x70, 0x20, 0x20, 0x20, 0x70};
+    unsigned char j[5]={0x70, 0x88, 0x8,  0x8,  0x38};
+    unsigned char k[5]={0x88, 0x88, 0xF0, 0xA0, 0x90}; 
+    unsigned char l[5]={0xF8, 0x80, 0x80, 0x80, 0x80};
+    unsigned char m[5]={0x88, 0x88, 0xA8, 0xD8, 0x88};
+    unsigned char n[5]={0x98, 0xA8, 0xA8, 0xA8, 0xC8};
+    unsigned char gn[5]={0x98, 0xA8, 0xA8, 0xA8, 0xC8};         // es la ñ
+    unsigned char o[5]={0x70, 0x88, 0x88, 0x88, 0x70};
+    unsigned char p[5]={0x80, 0x80, 0x78, 0x88, 0x78};
+    unsigned char q[5]={0x78, 0x90, 0x90, 0x90, 0x60};
+    unsigned char r[5]={0x88, 0x90, 0xF0, 0x88, 0xF0};    
+    unsigned char s[5]={0xF0, 0x8,  0x70, 0x80, 0x78};
+    unsigned char t[5]={0x20, 0x20, 0x20, 0x20, 0xF8};
+    unsigned char u[5]={0x70,0x88,0x88,0x88,0x88};
+    unsigned char v[5]={0x20,0x50,0x50,0x88,0x88};
+    unsigned char w[5]={0x88,0xD8,0xA8,0x88,0x88};
+    unsigned char x[5]={0x88,0x50,0x20,0x50,0x88};
+    unsigned char y[5]={0x20,0x20,0x70,0x88,0x88};
+    unsigned char z[5]={0xF8,0x80,0x70,0x8,0xF8};
         
-        //Construyendo la matriz para la letra a:
-        case 'a':
-            //columna 0
-            fill_pixel(vector, 0,0, r, g, b);
-            fill_pixel(vector, 0,1, r, g, b);
-            fill_pixel(vector, 0,2, r, g, b);
-            fill_pixel(vector, 0,3, r, g, b);
-            //columna 1            
-            fill_pixel(vector, 1,2, r, g, b);
-            fill_pixel(vector, 1,4, r, g, b);            
-            //columna 2
-            fill_pixel(vector, 2,2, r, g, b);
-            fill_pixel(vector, 2,4, r, g, b);
-            //columna 3
-            fill_pixel(vector, 3,2, r, g, b);
-            fill_pixel(vector, 3,4, r, g, b);
-            //columna 4
-            fill_pixel(vector, 4,0, r, g, b);
-            fill_pixel(vector, 4,1, r, g, b);
-            fill_pixel(vector, 4,2, r, g, b);
-            fill_pixel(vector, 4,3, r, g, b);
-            break;
-            
-        case 'e':
-            //columna 0
-            fill_pixel(vector, 0,0, r, g, b);
-            fill_pixel(vector, 0,1, r, g, b);
-            fill_pixel(vector, 0,2, r, g, b);
-            fill_pixel(vector, 0,3, r, g, b);
-
-            break;
-                        
-            
-        default:
-            break;
-    }
     
+    //unsigned char l0[0]=
     
+        
+    //Montamos un vector de vectores:
+    unsigned char *vectorPunteros []={a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z};
+            
+    //Con la letra recibida le restamos 26 y tenemos la posicion del vector.                        
+    //Para moverlo por los vectores.  
+    unsigned char *puntero; 
+    if (letra==32) //Se trata de un espacio         
+        puntero=espacio;
+    else              
+        //a= al valor 97
+        puntero=vectorPunteros[(letra-'a')];
+    
+        
+            //Vamos a recorrer todo el vector de bytes
+            for(int i=0; i<5; i++){
+                
+                unsigned char elemento = puntero[i];
+                unsigned int mask=0x80;
+                //Nos movemos por 5 bits de los 8
+                for(int x=0; x<5; x++){
+                    if (elemento & mask)
+                        fill_pixel(vector, x,i, red, green, blue);                                                                
+                    //Desplazamos 1 bit
+                    mask >>=1;
+                }                
+            }          
 }    
 
+void iluminaTexto(char cadena[], neopixel::PixelArray array){
+      
+       //Creamos un vector de pixeles.
+       neopixel::Pixel letra[25];
+        //Inicializamos el vector a 0
+        void_matrix(letra);
+        /*
+        for(int i=0; i<25; i++){
+            letra[i].red=0;
+            letra[i].blue=0;
+            letra[i].green=0;            
+        }*/
+        
+        for(int i=0; i<strlen(cadena); i++){
+            //Generamos la letra en el vector letra        
+            generaLetra(letra, cadena[i], 30, 0 , 0 );
+            //Iluminamos la matriz
+            array.update(letra, 25);
+            //Esperamos un seg
+            wait_ms(300); 
+            //Reseteamos la matriz
+            void_matrix(letra);
+            /*
+            for(int j=0; j<25; j++){
+                letra[j].red=0;
+                letra[j].blue=0;
+                letra[j].green=0;            
+            }
+            */
+            //Iluminamos la matriz a 0 (no la iluminamos = "la apagamos")
+            array.update(letra, 25);
+            //Esperamos otro segundo.                                    
+            wait_ms(30);   
+        }
+        wait_ms(600);
+        
+        
+}    
 
 int main()
 {
@@ -75,38 +145,11 @@ int main()
 
     // The pixel array control class.
     //Se está creando un objeto de tipo PixelArray y se está nombrando como array a la que se le está pasando DATA_PIN
-    neopixel::PixelArray array(DATA_PIN);
-
-    uint32_t offset = 0;
-    uint32_t i = 1;
-
-        neopixel::Pixel letra[25];
-        //Inicializamos el vector a 0
-        for(int i=1; i<25; i++){
-            letra[i].red=0;
-            letra[i].blue=0;
-            letra[i].green=0;            
-        } 
-        //Generamos la letra
-        generaLetra(letra, 'a', 150, 0 , 0 );
-        
+    neopixel::PixelArray array(DATA_PIN); 
     
-    while (1) {
-        //Generamos la letra en el vector letra        
-        generaLetra(letra, 'a', 150, 0 , 0 );
-        //Iluminamos la matriz
-        array.update(letra, 25);
-        //Esperamos un seg
-        wait_ms(1000); 
-        //Reseteamos la matriz
-        for(int i=0; i<25; i++){
-            letra[i].red=0;
-            letra[i].blue=0;
-            letra[i].green=0;            
-        }
-        //Iluminamos la matriz a 0 (no la iluminamos)
-        array.update(letra, 25);
-        //Esperamos otro segundo.                                    
-        wait_ms(1000);        
+    char greeting[] = "abcdefghijklmnopqrstuvwxyz";
+
+    while (1) {        
+        iluminaTexto(greeting, array);
     }
 }
