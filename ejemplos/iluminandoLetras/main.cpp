3f34aa8d2ccb678e74161b151d3581d3f3a40761
javacasm <javacasm@gmail.com>
2015-11-30 14:19:40 +0100

    url

--- ejemplos/iluminandoLetras/main.cpp
+++ ejemplos/iluminandoLetras/main.cpp
@@ -1,24 +1,26 @@
-//Código para la  #hackminiblip 
+//Código para la  #hackminiblip
 
 #include "mbed.h"
 #include "neopixel.h"
+
+// Otra forma de hacerlo usando librerías externas https://tequals0.wordpress.com/2014/05/22/scrolling-neopixel-leds/
 
 // Matrix led output pin
 #define DATA_PIN P0_9
 
 void fill_pixel(neopixel::Pixel buffer[25], int x, int y, int red, int green, int blue){
-    
+
     if(x<0) x=0;
     if(x>4) x=4;
     if(y<0) y=0;
     if(y>4) y=4;
-       
+
 
     int posicion=x+y*5;
     buffer[posicion].red=red;
     buffer[posicion].green=green;
     buffer[posicion].blue=blue;
-    
+
 }
 //r: intensidadRojo
 //g: intensidadVerde
@@ -26,7 +28,7 @@ void fill_pixel(neopixel::Pixel buffer[25], int x, int y, int red, int green, in
 void generaLetra(neopixel::Pixel vector[], char letra, int r, int g, int b){
 
     switch(letra){
-        
+
         //Construyendo la matriz para la letra a:
         case 'a':
             //columna 0
@@ -34,9 +36,9 @@ void generaLetra(neopixel::Pixel vector[], char letra, int r, int g, int b){
             fill_pixel(vector, 0,1, r, g, b);
             fill_pixel(vector, 0,2, r, g, b);
             fill_pixel(vector, 0,3, r, g, b);
-            //columna 1            
+            //columna 1
             fill_pixel(vector, 1,2, r, g, b);
-            fill_pixel(vector, 1,4, r, g, b);            
+            fill_pixel(vector, 1,4, r, g, b);
             //columna 2
             fill_pixel(vector, 2,2, r, g, b);
             fill_pixel(vector, 2,4, r, g, b);
@@ -49,7 +51,7 @@ void generaLetra(neopixel::Pixel vector[], char letra, int r, int g, int b){
             fill_pixel(vector, 4,2, r, g, b);
             fill_pixel(vector, 4,3, r, g, b);
             break;
-            
+
         case 'e':
             //columna 0
             fill_pixel(vector, 0,0, r, g, b);
@@ -58,14 +60,14 @@ void generaLetra(neopixel::Pixel vector[], char letra, int r, int g, int b){
             fill_pixel(vector, 0,3, r, g, b);
 
             break;
-                        
-            
+
+
         default:
             break;
     }
-    
-    
-}    
+
+
+}
 
 
 int main()
@@ -85,28 +87,28 @@ int main()
         for(int i=1; i<25; i++){
             letra[i].red=0;
             letra[i].blue=0;
-            letra[i].green=0;            
-        } 
+            letra[i].green=0;
+        }
         //Generamos la letra
         generaLetra(letra, 'a', 150, 0 , 0 );
-        
-    
+
+
     while (1) {
-        //Generamos la letra en el vector letra        
+        //Generamos la letra en el vector letra
         generaLetra(letra, 'a', 150, 0 , 0 );
         //Iluminamos la matriz
         array.update(letra, 25);
         //Esperamos un seg
-        wait_ms(1000); 
+        wait_ms(1000);
         //Reseteamos la matriz
         for(int i=0; i<25; i++){
             letra[i].red=0;
             letra[i].blue=0;
-            letra[i].green=0;            
+            letra[i].green=0;
         }
         //Iluminamos la matriz a 0 (no la iluminamos)
         array.update(letra, 25);
-        //Esperamos otro segundo.                                    
-        wait_ms(1000);        
+        //Esperamos otro segundo.
+        wait_ms(1000);
     }
 }
