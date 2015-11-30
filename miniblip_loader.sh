1ea09d348431c810b0522a74596f8634add4d51d
Andrés <demiurgosoft@hotmail.com>
2015-11-30 13:51:51 +0100

    another small change

--- miniblip_loader.sh
+++ miniblip_loader.sh
@@ -5,7 +5,7 @@
 
 if [ $# != 1 ]
 then
-	echo "Usage: bliploader + [firmaware.bin]";
+	echo "Usage: bash miniblip_loader + firmware.bin";
 else
 	port=$(mount | grep "CRP DISABLD" | grep -Eo "^[^ ]+");
 	if [ "$port" == "" ]
@@ -19,6 +19,5 @@ else
 	else
 		sudo dd if=$1 of=$port bs=512 seek=4 conv=notrunc
 		sudo umount $port
-		echo "loaded"
 	fi
 fi
